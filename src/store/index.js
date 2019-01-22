5c090b6ed82afb477b7136299c783ba7f9fed3c6
zhuoxin10 <gloriarzx@163.com>
2019-01-22 22:17:27 +0800

    删除条件

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,17 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import tagsView from './modules/tagsView'
-import getters from './getters'
+import Vue from "vue";
+import Vuex from "vuex";
+import tagsView from "./modules/tagsView";
+import cohortView from "./modules/cohortView";
+import getters from "./getters";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 const store = new Vuex.Store({
   modules: {
-    tagsView
+    tagsView,
+    cohortView
   },
   getters
-})
+});
 
-export default store
+export default store;
